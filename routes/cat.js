1a8914623d7a2aba6ea377b96ec15d791d088c2a
Kosuke Oshimoto <2301100056bs@cyber-u.ac.jp>
2026-01-25 20:28:21 +0900

    add env

--- routes/cat.js
+++ routes/cat.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const cors = require('cors');
+
+router.use(cors());
+
 router.get('/', async (req, res) => {
 request('https://api.thecatapi.com/v1/images/search', function (error, response,body) {
 if (!error && response.statusCode == 200) {
@@ -9,4 +13,5 @@ res.json(data);
 }
 });
 })
+
 module.exports = router;
